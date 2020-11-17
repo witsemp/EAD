780bf61990cab86b582edfc5f0245680632b7de4
Witold Sempruch <witsemp@gmail.com>
2020-11-17 21:49:52 +0100

    Tasks 1-3

--- Projekt/main.py
+++ Projekt/main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pandasgui
 import numpy as np
 from matplotlib import pyplot as plt
-import glob
 import os
 
 
@@ -15,8 +14,21 @@ def task1():
     for i, df in enumerate(dataframes):
         df['Year'] = years[i]
     df = pd.concat(dataframes)
-    pandasgui.show(df, settings={'block': True})
+    return df
 
+def task2(df: pd.DataFrame):
+    print("Number of unique names, regardless of sex: ", df['Name'].nunique())
 
+def task3(df: pd.DataFrame):
+    unique_male = df.loc[df['Sex'] == 'M', 'Name'].agg(['nunique']).iloc[0]
+    unique_female = df.loc[df['Sex'] == 'F', 'Name'].agg(['nunique']).iloc[0]
+    male_names = df.loc[df['Sex'] == 'M', ['Name', 'Year']]
+    female_names = df.loc[df['Sex'] == 'F', ['Name', 'Year']]
+    pandasgui.show(male_names, settings={'block': True})
+    pandasgui.show(female_names, settings={'block': True})
+    print("Number of unique female names: ", unique_female)
+    print("Number of unique male names: ", unique_male)
 if __name__ == '__main__':
-    task1()
+    df = task1()
+    task2(df)
+    task3(df)
