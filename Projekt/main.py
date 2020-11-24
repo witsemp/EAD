5943e582de69731054b6a8c7e1eca4725823e032
Witold Sempruch <witsemp@gmail.com>
2020-11-25 00:02:03 +0100

    It's done

--- Projekt/main.py
+++ Projekt/main.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import pandasgui
 import numpy as np
 from matplotlib import pyplot as plt
 import os
-import time
 import sqlite3
 
 years = [year for year in range(1880, 2020)]
@@ -19,31 +17,33 @@ def task1():
         df['Year'] = years[i]
     df = pd.concat(dataframes)
     df.index = np.arange(start=0, stop=len(df.index))
+    print("Zadanie 1: Tablica z danymi")
+    print(df)
     return df
 
 
 def task2(df: pd.DataFrame):
-    print("Number of unique names, regardless of Gender: ", df['Name'].nunique())
+    print("Zadanie 2: Ilość unikalnych imion nadanych w tym czasie to:  ", df['Name'].nunique())
 
 
 def task3(df: pd.DataFrame):
     unique_male = df.loc[df['Gender'] == 'M', 'Name'].agg(['nunique']).iloc[0]
     unique_female = df.loc[df['Gender'] == 'F', 'Name'].agg(['nunique']).iloc[0]
-    print("Number of unique female names: ", unique_female)
-    print("Number of unique male names: ", unique_male)
+    print("Zadanie 3: Ilość unikalnych imion żeńskich nadanych w tym czasie to: ", unique_female)
+    print("Zadanie 3: Ilość unikalnych imion męskich nadanych w tym czasie to: ", unique_male)
 
 
-# TODO: rebuild task 4
 def task4(df: pd.DataFrame):
     df['frequency_male'] = 0
     df['frequency_female'] = 0
+    df['frequency_male'] = df['frequency_male'].astype(np.float64)
+    df['frequency_female'] = df['frequency_female'].astype(np.float64)
     for year in years:
         total_yearly_male_births = df.loc[(df['Gender'] == 'M') & (df['Year'] == year), 'Count'].sum()
         total_yearly_female_births = df.loc[(df['Gender'] == 'F') & (df['Year'] == year), 'Count'].sum()
         df.loc[(df['Gender'] == 'M') & (df['Year'] == year), 'frequency_male'] = df['Count'] / total_yearly_male_births
         df.loc[(df['Gender'] == 'F') & (df['Year'] == year), 'frequency_female'] = df[
                                                                                        'Count'] / total_yearly_female_births
-
     return df
 
 
@@ -56,13 +56,13 @@ def task5(df: pd.DataFrame):
     years_as_rows_names['F/M'] = years_as_rows_names['Female births'] / years_as_rows_names['Male births']
     years_as_rows_names['Difference'] = np.abs(
         years_as_rows_names['Female births'] - years_as_rows_names['Male births'])
-    print("Year with biggest difference: ", years_as_rows_names['Difference'].idxmax())
-    print("Year with smallest difference: ", years_as_rows_names['Difference'].idxmin())
+    print("Zadanie 5: Rok z największą różnicą: ", years_as_rows_names['Difference'].idxmax())
+    print("Zadanie 5: Rok z najmniejszą różnicą: ", years_as_rows_names['Difference'].idxmin())
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
-    ax1.plot(years_as_rows_names.index.values, years_as_rows_names['Births'].values, 'or')
-    ax1.set_title('Total number of births per year')
-    ax2.plot(years_as_rows_names.index.values, years_as_rows_names['F/M'].values, 'or')
-    ax2.set_title('Female to male births ratio per year')
+    ax1.plot(years_as_rows_names.index.values, years_as_rows_names['Births'].values, 'r')
+    ax1.set_title('Całkowita roczna liczba urodzin')
+    ax2.plot(years_as_rows_names.index.values, years_as_rows_names['F/M'].values, 'r')
+    ax2.set_title('Stosunek liczby narodzin dziewczynek do liczby narodzin chłopców')
     fig.tight_layout()
     plt.show()
     return years_as_rows_names
@@ -81,6 +81,10 @@ def task6(df: pd.DataFrame):
     f1000_names_per_year = pd.DataFrame(f1000_names_per_year).rename(columns={0: "Top 1000 count"})
     male_ranking = male_df.sum(axis=0).sort_values(axis=0, ascending=False).head(1000)
     female_ranking = female_df.sum(axis=0).sort_values(axis=0, ascending=False).head(1000)
+    print("Zadanie 6: Ranking imion męskich")
+    print(pd.DataFrame(male_ranking))
+    print("Zadanie 6: Ranking imion żeńskich")
+    print(pd.DataFrame(female_ranking))
     return male_ranking, female_ranking, m1000_names_per_year, f1000_names_per_year
 
 
@@ -95,15 +99,15 @@ def task7(df: pd.DataFrame, m_ranking: pd.DataFrame, f_ranking: pd.DataFrame):
     def _format_ax(ax, name: str, row: int, col: int, frame_f: pd.DataFrame, frame_s: pd.DataFrame):
         ax[row][col].plot(frame_s.index.values, frame_s.values)
         ax[row][col].set_title(name)
-        ax[row][col].set_ylabel('Total number change')
+        ax[row][col].set_ylabel('Całkowita roczna liczba urodzeń')
         secax = ax[row][col].twinx()
         secax.plot(frame_f.index.values, frame_f.values, color="red")
-        secax.set_ylabel('Frequency change')
+        secax.set_ylabel('Roczna popularność')
 
     top_male_name = m_ranking.head(1).index[0]
-    print("Most common male name: ", top_male_name)
+    print("Zadanie 7: Najpopularniejsze imię męskie: ", top_male_name)
     top_female_name = f_ranking.head(1).index[0]
-    print("Most common female name: ", top_female_name)
+    print("Zadanie 7: Najpopularniejsze imię żeńskie: ", top_female_name)
     harry_sum = _get_sum_for_name('Harry', df)
     harry_f = _get_frequency_for_name('Harry', df)
     marilyn_sum = _get_sum_for_name('Marilin', df)
@@ -112,6 +116,21 @@ def task7(df: pd.DataFrame, m_ranking: pd.DataFrame, f_ranking: pd.DataFrame):
     top_male_frequency = _get_frequency_for_name(top_male_name, df)
     top_female_sum = _get_sum_for_name(top_female_name, df)
     top_female_frequency = _get_frequency_for_name(top_female_name, df)
+    frame_names = df.pivot_table(index='Name', columns='Year', values='Count', aggfunc='sum')
+    count_harry = frame_names.loc['Harry', [1940, 1980, 2019]].values
+    count_marilin = frame_names.loc['Marilin', [1940, 1980, 2019]].values
+    count_marilin[0] = 0
+    count_top_male = frame_names.loc[top_male_name, [1940, 1980, 2019]].values
+    count_top_female = frame_names.loc[top_female_name, [1940, 1980, 2019]].values
+    print("Zadanie 7: Imię Harry w latach 1940, 1980, 2019 nadane odpowiednio ", count_harry[0], count_harry[1],
+          count_harry[2], ' razy')
+    print("Zadanie 7: Imię Marilin w latach 1940, 1980, 2019 nadane odpowiednio ", count_marilin[0], count_marilin[1],
+          count_marilin[2], ' razy')
+    print("Zadanie 7: Imię James w latach 1940, 1980, 2019 nadane odpowiednio ", count_top_male[0], count_top_male[1],
+          count_top_male[2], ' razy')
+    print("Zadanie 7: Imię Mary w latach 1940, 1980, 2019 nadane odpowiednio ", count_top_female[0],
+          count_top_female[1],
+          count_top_female[2], ' razy')
     fig, ax = plt.subplots(2, 2)
     _format_ax(ax, 'Harry', 0, 0, harry_f, harry_sum)
     _format_ax(ax, 'Marilin', 0, 1, marilyn_f, marilyn_sum)
@@ -122,13 +141,16 @@ def task7(df: pd.DataFrame, m_ranking: pd.DataFrame, f_ranking: pd.DataFrame):
 
 
 def task8(frame: pd.DataFrame, m_ranking, f_ranking):
-    frame['Top 1000 male ratio'] = m_ranking['Top 1000 count'] / frame['Male births']
-    frame['Top 1000 female ratio'] = f_ranking['Top 1000 count'] / frame['Female births']
+    frame['Top 1000 male ratio'] = (m_ranking['Top 1000 count'] / frame['Male births'])*100.0
+    frame['Top 1000 female ratio'] = (f_ranking['Top 1000 count'] / frame['Female births'])*100.0
+    frame['Top 1000 difference'] = np.abs(frame['Top 1000 male ratio'] - frame['Top 1000 female ratio'])
+    max_difference_year = frame['Top 1000 difference'].idxmax()
+    print("Zadanie 8: Rok, w którym zaobserwowano największą różnicę w różnorodności: ", max_difference_year)
     fig, ax = plt.subplots()
     ax.plot(frame.index.values, frame['Top 1000 male ratio'].values, color='blue')
     ax.plot(frame.index.values, frame['Top 1000 female ratio'].values, color='red')
-    ax.legend(['Top 1000 names male ratio', 'Top 1000 names female ratio'])
-    ax.set_title("Top 1000 names to all names ratio by year")
+    ax.legend(['Udział imion męskich', 'Udział imion żeńskich'])
+    ax.set_title("Roczny procentowy udział imion z rankingu top 1000")
     plt.show()
 
 
@@ -136,31 +158,35 @@ def task9(frame: pd.DataFrame, frame_statistics: pd.DataFrame):
     frame['Last letter'] = df['Name'].str.strip().str[-1]
     pivot_frame = frame.pivot_table(index=['Year'], columns=['Last letter', 'Gender'], values='Count', aggfunc='sum')
     pivot_frame = pivot_frame.div(pivot_frame.sum(axis=1), axis=0)
-    # TODO: Check whether sum over both genders
     all_years = pivot_frame.copy()
     pivot_frame = pivot_frame.loc[[1910, 1960, 2015], :]
     pivot_frame = pivot_frame.iloc[:, pivot_frame.columns.get_level_values(1) == 'M']
     pivot_frame.columns = pivot_frame.columns.droplevel(1)
     pivot_frame = pivot_frame.rename_axis(None, axis=1)
-    ax = pivot_frame.T.plot(kind='bar')
-    # TODO: Change xticks rotation
+    ax1 = pivot_frame.T.plot(kind='bar')
+    ax1.set_title("Popularność ostatniej litery imienia dla mężczyzn z podziałem na lata")
     plt.show()
     pivot_frame = pivot_frame.transpose()
     pivot_frame['Change over period'] = np.abs(pivot_frame[2015] - pivot_frame[1910])
+    pivot_frame['Change with sign'] = pivot_frame[2015] - pivot_frame[1910]
     pivot_frame.sort_values(by='Change over period', axis=0, ascending=False, inplace=True)
+    max_increase = pivot_frame['Change with sign'].idxmax()
+    max_decrease = pivot_frame['Change with sign'].idxmin()
+    print('Zadanie 9: Maksymalny przyrost wystąpił dla litery: ', max_increase)
+    print('Zadanie 9: Maksymalny spadek wystąpił dla litery: ', max_decrease)
     most_popular_letters = pivot_frame.head(3).index.values
-    # print("Letter with biggest change between 2015 and 1910 is: ", most_popular_letters[0])
+    print('Zadanie 9: Największy wzrost/spadek wystąpił dla litery: ', most_popular_letters[0])
     all_years = all_years.iloc[:, all_years.columns.get_level_values(1) == 'M']
     all_years.columns = all_years.columns.droplevel(1)
     all_years = all_years.rename_axis(None, axis=1)
     all_years = all_years.transpose()
     all_years = all_years.loc[most_popular_letters, :]
-    print(all_years)
-    # TODO: Add description to plot
     fig, ax = plt.subplots()
     ax.plot(all_years.columns.values, all_years.loc['n', :], color='red')
     ax.plot(all_years.columns.values, all_years.loc['r', :], color='green')
     ax.plot(all_years.columns.values, all_years.loc['d', :], color='blue')
+    ax.set_title("Przebieg trendu popularności w maksymalnym przedziale czasu")
+    ax.legend(['n', 'r', 'd'])
     plt.show()
 
 
@@ -192,12 +218,12 @@ def task11(frame: pd.DataFrame):
     merged['Popularity difference'] = np.abs(merged['Popularity_x'] - merged['Popularity_y'])
     merged.sort_values(by='Popularity difference', ascending=False, inplace=True)
     names = merged.head(2).index.values
+    print("Zadanie 11: Najpopularniejsze imiona: ", names)
     frame_copy = frame.reset_index()
     frame_copy = frame_copy.loc[:, ['Year', 'Name', 'Count_x', 'Count_y']]
     frame_copy = frame_copy.set_index('Name')
     name1_df = frame_copy.loc[names[0], :]
     name2_df = frame_copy.loc[names[1], :]
-    fig, (ax1, ax2) = plt.subplots(1, 2)
 
 
 def task12():
@@ -212,30 +238,25 @@ def task13(frame1: pd.DataFrame, frame2: pd.DataFrame):
     frame1_pivot = pd.DataFrame(frame1_queried).pivot_table(index='Year', values='dx', aggfunc='sum')
     frame2_queried = pd.DataFrame(frame2.loc[1959:2017, 'Births'])
     frame2_queried.columns = ['Births']
-    print(frame1_pivot)
-    print(frame2_queried)
     frame2_queried['Difference'] = frame2_queried['Births'] - frame1_pivot['dx']
     rate_sum = frame2_queried['Difference'].sum()
     rate_mean = frame2_queried['Difference'].mean()
-    print("Całkowity przyrost w okresie 1959-2017 wynosi: ", rate_sum)
-    print("Średni przyrost w okresie 1959-2017 wynosi: ", rate_mean)
+    print("Zadanie 13: Całkowity przyrost w okresie 1959-2017 wynosi: ", rate_sum)
+    print("Zadanie 13: Średni przyrost w okresie 1959-2017 wynosi: ", rate_mean)
     return frame2_queried
 
 
 def task14(frame1: pd.DataFrame, frame2: pd.DataFrame):
-    print(frame1)
     frame_queried = frame1.query("Age == 0").loc[:, ['Year', 'dx']]
     frame_queried = pd.DataFrame(frame_queried).pivot_table(index='Year', values='dx', aggfunc='sum')
-    print(frame_queried)
-    print(frame2)
     frame2['Survival rate'] = (frame2['Births'] - frame_queried['dx']) / frame2['Births']
-    print(frame2)
     fig, ax = plt.subplots()
+    ax.set_title("Wartość współczynnika przeżywalności dzieci")
     ax.plot(frame2.index.values, frame2['Survival rate'].values)
-    plt.show()
+    return ax
 
 
-def task15(frame1: pd.DataFrame, frame2: pd.DataFrame):
+def task15(frame1: pd.DataFrame, frame2: pd.DataFrame, axis):
     def _get_5_year_period_mortality(year, df1: pd.DataFrame):
         mortality_sum = 0
         df_queried = pd.DataFrame(df1.loc[((df1['Year'] >= year) & (df1['Year'] <= year + 4)), :])
@@ -246,31 +267,25 @@ def task15(frame1: pd.DataFrame, frame2: pd.DataFrame):
 
     fq = pd.DataFrame(frame1.loc[:, ['Year', 'Age', 'Sex', 'dx']])
     aggregated_mortality = [_get_5_year_period_mortality(year, fq) for year in years_mortality]
-    survival_ratio = (frame2['Births'].values - aggregated_mortality)/frame2['Births'].values
-    fig, ax = plt.subplots()
-    ax.plot(years_mortality, survival_ratio)
-    print(survival_ratio)
+    survival_ratio = (frame2['Births'].values - aggregated_mortality) / frame2['Births'].values
+    axis.plot(years_mortality, survival_ratio)
+    axis.legend(['Przeżywalność w pierwszym roku życia', "Przeżywalność w pierwszych 5 latach życia"])
     plt.show()
 
 
-
 if __name__ == '__main__':
-    # start_time = time.time()
     df = task1()
-    # task2(df)
-    # task3(df)
+    task2(df)
+    task3(df)
     df_frequency = task4(df)
     df_frequency_pivot = task5(df_frequency)
-    print(df_frequency_pivot)
     male_ranking, female_ranking, m1000_per_year, f_1000_per_year = task6(df_frequency)
-    # task7(df_frequency, male_ranking, female_ranking)
-    # task8(df_frequency_pivot, m1000_per_year, f_1000_per_year)
+    task7(df_frequency, male_ranking, female_ranking)
+    task8(df_frequency_pivot, m1000_per_year, f_1000_per_year)
     task9(df_frequency, df_frequency_pivot)
     df_fm_names = task10(df_frequency)
-    # task11(df_fm_names)
+    task11(df_fm_names)
     sql_frame = task12()
     df_births_by_year = task13(sql_frame, df_frequency_pivot)
-    task14(sql_frame, df_births_by_year)
-    task15(sql_frame, df_births_by_year)
-
-    # print(time.time() - start_time)
+    axs = task14(sql_frame, df_births_by_year)
+    task15(sql_frame, df_births_by_year, axs)
